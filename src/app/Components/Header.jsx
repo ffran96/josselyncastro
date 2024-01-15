b06e15721785db9e36ec33d38da83ac7dc62bb32
Fran <=>
2024-01-15 22:17:20 +0100

    Header bg

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -5,10 +5,10 @@ const Enlaces = ["Conóceme", "Curriculum", "Book", "VideoBook", "Contacto"];
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-around fixed top-0 w-full px-6 py-4 z-50">
+    <header className="flex items-center justify-around fixed top-0 w-full px-6 py-4 z-50  bg-[#fff]/10">
       <Logo />
       <nav>
-        <ul className="fixed w-full h-dvh inset-0 bg-[#151a36]/90 text-2xl flex flex-col items-center justify-center gap-8 -translate-y-full transition-transform duration-300 target:translate-y-0 md:static md:h-[initial] md:bg-[initial] md:text-xl md:flex-row md:translate-y-[initial]">
+        <ul className="fixed w-full h-dvh inset-0 text-2xl flex flex-col items-center justify-center gap-8 -translate-y-full transition-transform duration-300 target:translate-y-0 md:static md:h-[initial] md:bg-[initial] md:text-xl md:flex-row md:translate-y-[initial]">
           <li>Conóceme</li>
           <li>Curriculum</li>
           <li>Book</li>
