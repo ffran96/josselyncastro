4a30c829411edcc036af0897ff3014786b053266
Fran <=>
2024-01-15 21:56:26 +0100

    First Commit

---
+++ src/app/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Logo from "./Logo";
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-around fixed top-0 w-full px-6 py-4 z-50 bg-slate-600/40">
+      <Logo />
+      <nav>
+        <ul className="fixed w-full h-dvh inset-0 bg-[#151a36]/90 text-2xl flex flex-col items-center justify-center gap-8 -translate-y-full transition-transform duration-300 target:translate-y-0 md:static md:h-[initial] md:bg-[initial] md:text-xl md:flex-row md:translate-y-[initial]">
+            <li>Conóceme</li>
+            <li>Curriculum</li>
+            <li>Book</li>
+            <li>VideoBook</li>
+            <li>Contacto</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ src/app/Components/Logo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <span className='text-2xl'>Josselyn Castro</span>
+  )
+}
