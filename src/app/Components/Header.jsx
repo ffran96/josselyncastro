68e0bd1dd87021c86c13ccb6180c78de9b1b1d3e
Fran <=>
2024-01-15 22:13:52 +0100

    header wallpaper

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Logo from "./Logo";
 
+const Enlaces = ["Conóceme", "Curriculum", "Book", "VideoBook", "Contacto"];
+
 export default function Header() {
   return (
-    <header className="flex items-center justify-around fixed top-0 w-full px-6 py-4 z-50 bg-slate-600/40">
+    <header className="flex items-center justify-around fixed top-0 w-full px-6 py-4 z-50">
       <Logo />
       <nav>
         <ul className="fixed w-full h-dvh inset-0 bg-[#151a36]/90 text-2xl flex flex-col items-center justify-center gap-8 -translate-y-full transition-transform duration-300 target:translate-y-0 md:static md:h-[initial] md:bg-[initial] md:text-xl md:flex-row md:translate-y-[initial]">
-            <li>Conóceme</li>
-            <li>Curriculum</li>
-            <li>Book</li>
-            <li>VideoBook</li>
-            <li>Contacto</li>
+          <li>Conóceme</li>
+          <li>Curriculum</li>
+          <li>Book</li>
+          <li>VideoBook</li>
+          <li>Contacto</li>
         </ul>
       </nav>
     </header>
