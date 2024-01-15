d19dbec3b0caf26052dd9b67930f761af21907f0
Fran <=>
2024-01-15 23:54:58 +0100

    Secciones

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import Logo from "./Logo";
+import Link from "next/link";
 
 const Enlaces = [
-  { id: "0", name: "Conóceme", link: "/conoceme" },
-  { id: "1", name: "Curriculum", link: "/cv" },
-  { id: "2", name: "Book", link: "/book" },
-  { id: "3", name: "VideoBook", link: "/vbook" },
-  { id: "4", name: "Contacto", link: "/contacto" },
+  { id: "0", name: "Conóceme", link: "#conoceme" },
+  { id: "1", name: "Curriculum", link: "#cv" },
+  { id: "2", name: "Book", link: "#book" },
+  { id: "3", name: "VideoBook", link: "#vbook" },
+  { id: "4", name: "Contacto", link: "#contacto" },
 ];
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-around fixed top-0 w-full px-6 py-4 z-50">
-      <Logo />
+    <header className="flex items-center justify-around fixed top-0 w-full px-6 py-4 z-50 bg-white/5 backdrop-blur-lg">
+      <Link href="/">
+        <Logo />
+      </Link>
       <nav>
         <ul className="fixed w-full h-dvh inset-0 text-2xl flex flex-col items-center justify-center gap-8 -translate-y-full transition-transform duration-300 target:translate-y-0 md:static md:h-[initial] md:bg-[initial] md:text-xl md:flex-row md:translate-y-[initial]">
           {Enlaces.map((item) => (
-            <li key={item.id}>{item.name}</li>
+            <Link key={item.id} href={item.link}>
+              {" "}
+              <li>{item.name}</li>{" "}
+            </Link>
           ))}
         </ul>
       </nav>
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,52 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="">
-      <img className="w-screen h-screen object-cover object-top"
-      src="/BI2A5681.jpg"
-      alt="Joss"
-       />
+      <img
+        className="w-screen h-screen object-cover object-top"
+        src="/BI2A5681.jpg"
+        alt="Joss"
+      />
+
+      <section id="conoceme" className="">
+        Conoceme
+        <img
+          className="w-screen h-screen object-cover object-top"
+          src="/BI2A5681.jpg"
+          alt="Joss"
+        />
+      </section>
+      <section id="cv" className="">
+        CV
+        <img
+          className="w-screen h-screen object-cover object-top"
+          src="/BI2A5681.jpg"
+          alt="Joss"
+        />
+      </section>
+      <section id="book" className="">
+        Book
+        <img
+          className="w-screen h-screen object-cover object-top"
+          src="/BI2A5681.jpg"
+          alt="Joss"
+        />
+      </section>
+      <section id="vbook" className="">
+        VBook
+        <img
+          className="w-screen h-screen object-cover object-top"
+          src="/BI2A5681.jpg"
+          alt="Joss"
+        />
+      </section>
+      <section id="contacto" className="">
+        Contacto
+        <img
+          className="w-screen h-screen object-cover object-top"
+          src="/BI2A5681.jpg"
+          alt="Joss"
+        />
+      </section>
     </main>
   );
 }
