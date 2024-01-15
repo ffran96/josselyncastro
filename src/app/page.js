85a08f1f71a2e4acdf801590103c74e58650452a
Fran <=>
2024-01-15 23:21:17 +0100

    bg-joss

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <img className="w-screen h-screen object-cover object-end"
+      <img className="w-screen h-screen object-cover object-top"
       src="/BI2A5681.jpg"
       alt="Joss"
        />
