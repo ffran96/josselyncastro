beff111f23034ff7956fbf34efe5f5b3fb4867e0
Fran <=>
2024-01-17 23:00:33 +0100

    Gallery

---
+++ src/app/Components/Gallery.jsx
@@ -0,0 +1,37 @@
+'use client';
+import React, { useEffect } from 'react';
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import 'photoswipe/style.css';
+
+export default function SimpleGallery(props) {
+  useEffect(() => {
+    let lightbox = new PhotoSwipeLightbox({
+      gallery: '#' + props.galleryID,
+      children: 'a',
+      pswpModule: () => import('photoswipe'),
+    });
+    lightbox.init();
+
+    return () => {
+      lightbox.destroy();
+      lightbox = null;
+    };
+  }, []);
+
+  return (
+    <div className="grid lg:grid-cols-6 gap-4 mx-10" id={props.galleryID}>
+      {props.images.map((image, index) => (
+        <a
+          href={image.largeURL}
+          data-pswp-width={image.width}
+          data-pswp-height={image.height}
+          key={props.galleryID + '-' + index}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={image.thumbnailURL} alt="" />
+        </a>
+      ))}
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,156 @@
+import Gallery from "./Components/Gallery";
+
+const GalleryImages = [
+  {
+    largeURL:
+      "/image (1).jpg",
+    thumbnailURL:
+      "/image (1).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (2).jpg",
+    thumbnailURL:
+      "/image (2).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (3).jpg",
+    thumbnailURL:
+      "/image (3).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (4).jpg",
+    thumbnailURL:
+      "/image (4).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (5).jpg",
+    thumbnailURL:
+      "/image (5).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (6).jpg",
+    thumbnailURL:
+      "/image (6).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (7).jpg",
+    thumbnailURL:
+      "/image (7).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (8).jpg",
+    thumbnailURL:
+      "/image (8).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (9).jpg",
+    thumbnailURL:
+      "/image (9).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (10).jpg",
+    thumbnailURL:
+      "/image (10).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (11).jpg",
+    thumbnailURL:
+      "/image (11).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (12).jpg",
+    thumbnailURL:
+      "/image (12).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (13).jpg",
+    thumbnailURL:
+      "/image (13).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (14).jpg",
+    thumbnailURL:
+      "/image (14).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (15).jpg",
+    thumbnailURL:
+      "/image (15).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (16).jpg",
+    thumbnailURL:
+      "/image (16).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (17).jpg",
+    thumbnailURL:
+      "/image (17).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  {
+    largeURL:
+      "/image (18).jpg",
+    thumbnailURL:
+      "/image (18).jpg",
+    width: 2500,
+    height: 3750,
+  },
+  
+];
+
 export default function Home() {
   return (
-    <main className="">
+    <main className="relative">
       <img
         className="w-screen h-screen object-cover object-top"
         src="/BI2A5681.jpg"
@@ -9,43 +159,23 @@ export default function Home() {
 
       <section id="conoceme" className="">
         Conoceme
-        <img
-          className="w-screen h-screen object-cover object-top"
-          src="/BI2A5681.jpg"
-          alt="Joss"
-        />
       </section>
       <section id="cv" className="">
         CV
-        <img
-          className="w-screen h-screen object-cover object-top"
-          src="/BI2A5681.jpg"
-          alt="Joss"
-        />
       </section>
       <section id="book" className="">
-        Book
-        <img
-          className="w-screen h-screen object-cover object-top"
-          src="/BI2A5681.jpg"
-          alt="Joss"
-        />
+        <div>
+          <Gallery
+            galleryID="gallery"
+            images={GalleryImages}
+          />
+        </div>
       </section>
       <section id="vbook" className="">
         VBook
-        <img
-          className="w-screen h-screen object-cover object-top"
-          src="/BI2A5681.jpg"
-          alt="Joss"
-        />
       </section>
       <section id="contacto" className="">
         Contacto
-        <img
-          className="w-screen h-screen object-cover object-top"
-          src="/BI2A5681.jpg"
-          alt="Joss"
-        />
       </section>
     </main>
   );
