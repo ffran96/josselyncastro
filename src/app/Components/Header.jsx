eb9aa9850ccf2469c39b7bd755ef667e4d0948fb
Fran <=>
2024-01-17 23:00:04 +0100

    Gallery

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -20,8 +20,7 @@ export default function Header() {
         <ul className="fixed w-full h-dvh inset-0 text-2xl flex flex-col items-center justify-center gap-8 -translate-y-full transition-transform duration-300 target:translate-y-0 md:static md:h-[initial] md:bg-[initial] md:text-xl md:flex-row md:translate-y-[initial]">
           {Enlaces.map((item) => (
             <Link key={item.id} href={item.link}>
-              {" "}
-              <li>{item.name}</li>{" "}
+              <li>{item.name}</li>
             </Link>
           ))}
         </ul>
