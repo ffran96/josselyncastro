2b8aad1dc1db8114a08931886677571adc908301
Fran <=>
2024-01-22 12:50:18 +0100

    Dropdown

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -17,26 +17,40 @@ const Enlaces = [
 export default function Header() {
   const [HandleClick, setHandleClick] = useState(false);
   return (
-    <header className="flex items-center justify-between fixed top-0 w-full px-6 md:px-24 py-4 z-50 bg-white/5 backdrop-blur-lg">
-      <Link href="/">
-        <Logo />
-      </Link>
-      <nav>
-        <ul className="fixed w-full h-dvh inset-0 text-2xl flex flex-col items-center justify-center gap-8 -translate-y-full transition-transform duration-300 target:translate-y-0 md:static md:h-[initial] md:bg-[initial] md:text-xl md:flex-row md:translate-y-[initial]">
-          {Enlaces.map((item) => (
-            <li className="text-slate-100" key={item.id}>
-              <Link href={item.link}>{item.name}</Link>
-            </li>
-          ))}
-        </ul>
-        <div
-          onClick={() => setHandleClick(!HandleClick)}
-          className="mobile-buttom-menu"
-          style={{ fontSize: "28px", color: "white" }}
-        >
-          <FontAwesomeIcon className="md:hidden " icon={HandleClick ? faXmark : faBars} />
+    <>
+      {HandleClick && (
+        <div className="flex fixed justify-center py-4 z-50 mt-16 w-full bg-slate-950 transition-all">
+          <ul className="space-y-6">
+            {Enlaces.map((item) => (
+              <li className="text-slate-100" key={item.id}>
+                <Link href={item.link}>{item.name}</Link>
+              </li>
+            ))}
+          </ul>
         </div>
-      </nav>
-    </header>
+      )}
+      <header className="flex items-center justify-between fixed top-0 w-full px-6 md:px-24 py-4 z-50 bg-white/5 backdrop-blur-lg">
+        <Link href="/">
+          <Logo />
+        </Link>
+        <nav>
+          {!HandleClick && (
+            <ul className="fixed w-full h-dvh inset-0 text-2xl flex flex-col items-center justify-center gap-8 -translate-y-full transition-transform duration-300 target:translate-y-0 md:static md:h-[initial] md:bg-[initial] md:text-xl md:flex-row md:translate-y-[initial]">
+              {Enlaces.map((item) => (
+                <li className="text-slate-100" key={item.id}>
+                  <Link href={item.link}>{item.name}</Link>
+                </li>
+              ))}
+            </ul>
+          )}
+          <div onClick={() => setHandleClick(!HandleClick)}>
+            <FontAwesomeIcon
+              className="md:hidden text-2xl text-slate-100 items-center"
+              icon={HandleClick ? faXmark : faBars}
+            />
+          </div>
+        </nav>
+      </header>
+    </>
   );
 }
