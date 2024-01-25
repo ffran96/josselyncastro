aea3a910293e988d5e72e254250b379b3a8329cd
Fran <=>
2024-01-25 22:03:09 +0100

    Estilos para los desplegables del CV

--- src/app/Components/LiCV.jsx
+++ src/app/Components/LiCV.jsx
@@ -11,23 +11,26 @@ export default function LiCV({ Titulo, Lugar, Fecha, Descripcion }) {
   const [HandleClick, setHandleClick] = useState(false);
 
   return (
-    <li className="lg:hover:bg-slate-400/20 rounded-lg p-2 transition-all ease-in-out cursor-pointer" onClick={() => setHandleClick(!HandleClick)}>
+    <li
+      className="lg:w-[450px] lg:hover:bg-slate-400/20 rounded-lg p-2 transition-all ease-in-out cursor-pointer"
+      onClick={() => setHandleClick(!HandleClick)}
+    >
       <div className="flex items-center gap-4">
         <FontAwesomeIcon
-        className="size-4 text-slate-80000"
+          className="size-4 text-slate-900"
           icon={!HandleClick ? faChevronRight : faChevronDown}
         />
         <div className="flex flex-col">
           <span className="text-xl font-medium">{Titulo}</span>
           <span className="italic text-base">{Lugar}</span>
           <span className="text-sm">{Fecha}</span>
+          {HandleClick && (
+            <ul>
+              <li>{Descripcion}</li>
+            </ul>
+          )}
         </div>
       </div>
-      {HandleClick && (
-        <ul>
-          <li>{Descripcion}</li>
-        </ul>
-      )}
     </li>
   );
 }
