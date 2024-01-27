7719271d9fc865d361e9f81d05b7b47155c60cd1
Fran <=>
2024-01-27 22:08:08 +0100

    framer-motion

--- src/app/Components/Dropdown.jsx
+++ src/app/Components/Dropdown.jsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Dropdown({ HandleClick, Enlaces, setHandleClick }) {
   return (
     <>
       {HandleClick && (
-        <div className="flex fixed left-0 top-16  justify-center py-4 z-50  w-full border-solid border-t-[1px] border-neutral-50/60 bg-black/90 backdrop-blur-lg">
+        <motion.div
+          initial={{ y: -313 }}
+          animate= {{y: 0}}
+          className="flex fixed left-0 top-16  justify-center py-4 z-50  w-full  bg-black/90 backdrop-blur-lg"
+        >
           <ul className="space-y-6">
             {Enlaces.map((item) => (
               <li className="text-slate-100" key={item.id}>
@@ -18,7 +23,7 @@ export default function Dropdown({ HandleClick, Enlaces, setHandleClick }) {
               </li>
             ))}
           </ul>
-        </div>
+        </motion.div>
       )}
     </>
   );
