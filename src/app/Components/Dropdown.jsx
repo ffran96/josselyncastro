24bc905b5c2818376a285149b291caa2443515c8
Fran <=>
2024-01-27 20:34:11 +0100

    Estilo del dropdown

--- src/app/Components/Dropdown.jsx
+++ src/app/Components/Dropdown.jsx
@@ -5,7 +5,7 @@ export default function Dropdown({ HandleClick, Enlaces, setHandleClick }) {
   return (
     <>
       {HandleClick && (
-        <div className="flex absolute left-0 top-16  justify-center py-4 z-50  w-full  bg-black/90 backdrop-blur-lg">
+        <div className="flex fixed left-0 top-16  justify-center py-4 z-50  w-full  bg-black/90 backdrop-blur-lg">
           <ul className="space-y-6">
             {Enlaces.map((item) => (
               <li className="text-slate-100" key={item.id}>
