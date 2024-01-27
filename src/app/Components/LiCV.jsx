a3df76b884db2bcf95bb81c900cb050a1cc0dd62
Fran <=>
2024-01-27 12:04:08 +0100

    Estilos

--- src/app/Components/LiCV.jsx
+++ src/app/Components/LiCV.jsx
@@ -15,17 +15,19 @@ export default function LiCV({ Titulo, Lugar, Fecha, Descripcion }) {
       className="lg:w-[450px] lg:hover:bg-slate-400/20 rounded-lg p-2 transition-all ease-in-out cursor-pointer"
       onClick={() => setHandleClick(!HandleClick)}
     >
-      <div className="flex items-center gap-4">
-        <FontAwesomeIcon
-          className="size-4 text-slate-900"
-          icon={!HandleClick ? faChevronRight : faChevronDown}
-        />
+      <div className="flex items-center  gap-4">
+        <span className="size-4 relative">
+          <FontAwesomeIcon
+            className="size-4 absolute inset-0 m-auto text-slate-900"
+            icon={!HandleClick ? faChevronRight : faChevronDown}
+          />
+        </span>
         <div className="flex flex-col">
           <span className="text-xl font-medium">{Titulo}</span>
           <span className="italic text-base">{Lugar}</span>
           <span className="text-sm">{Fecha}</span>
           {HandleClick && (
-            <ul className="mt-2">
+            <ul className="mt-2 px-2 w-full">
               <li>{Descripcion}</li>
             </ul>
           )}
