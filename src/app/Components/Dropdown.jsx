d7adc777770058ed8d0337de4b269c6b26093c97
Fran <=>
2024-01-27 22:45:42 +0100

    Animación dropdown!

--- src/app/Components/Dropdown.jsx
+++ src/app/Components/Dropdown.jsx
@@ -5,26 +5,24 @@ import { motion } from "framer-motion";
 export default function Dropdown({ HandleClick, Enlaces, setHandleClick }) {
   return (
     <>
-      {HandleClick && (
-        <motion.div
-          initial={{ y: -313 }}
-          animate= {{y: 0}}
-          className="flex fixed left-0 top-16  justify-center py-4 z-50  w-full  bg-black/90 backdrop-blur-lg"
-        >
-          <ul className="space-y-6">
-            {Enlaces.map((item) => (
-              <li className="text-slate-100" key={item.id}>
-                <Link
-                  onClick={() => setHandleClick(!HandleClick)}
-                  href={item.link}
-                >
-                  {item.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </motion.div>
-      )}
+      <motion.div
+        initial={HandleClick ? { y: -313 } : { y: 0 }}
+        animate={HandleClick ? { y: 0 } : { y: -313 }}
+        className="flex fixed left-0 top-16  justify-center py-4 z-40  w-full  bg-black/90 backdrop-blur-lg"
+      >
+        <ul className="space-y-6">
+          {Enlaces.map((item) => (
+            <li className="text-slate-100" key={item.id}>
+              <Link
+                onClick={() => setHandleClick(!HandleClick)}
+                href={item.link}
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </motion.div>
     </>
   );
 }
