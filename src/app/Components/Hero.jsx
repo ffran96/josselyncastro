f0f8725f2e920a1185668f4058de8c2a2183bf1c
Fran <=>
2024-01-31 21:27:53 +0100

    Quitar boton Hero

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -4,14 +4,12 @@ import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function Hero() {
   return (
-    <section id={"Hero"} className="relative select-none">
+    <section id={"Hero"} className="select-none">
       <img
         className=" w-screen h-screen object-cover object-top"
         src="/BI2A5681.jpg"
         alt="Joss"
       />
-
-      <FontAwesomeIcon className="absolute bottom-0 inset-0 m-auto mb-10 text-4xl z-10 opacity-60" icon={faAnglesDown} />
     </section>
   );
 }
