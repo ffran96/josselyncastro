720970b274122a8f7999e6cfdffd9c28109209a8
Fran <=>
2024-01-31 21:21:27 +0100

    Boton en Hero

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function Hero() {
   return (
-    <section id={"Hero"}>
+    <section id={"Hero"} className="relative select-none">
       <img
-        className="w-screen h-screen object-cover object-top"
+        className=" w-screen h-screen object-cover object-top"
         src="/BI2A5681.jpg"
         alt="Joss"
       />
+
+      <FontAwesomeIcon className="absolute bottom-0 inset-0 m-auto mb-10 text-4xl z-10 opacity-60" icon={faAnglesDown} />
     </section>
   );
 }
--- src/app/Components/VBook.jsx
+++ src/app/Components/VBook.jsx
@@ -16,6 +16,7 @@ const Videos = [
     id: "yVy_nbqCeG4",
     title: "LA DIOSA 1958 (ESCENA FINAL) - POR JOSSELYN CASTRO",
   },
+  
 ];
 
 export default function VBook() {
