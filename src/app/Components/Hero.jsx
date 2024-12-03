a9e57d1f5d6e4b05e82060a36046711e499066e9
Fran <=>
2024-12-03 22:33:47 +0100

    Implementación correo electronico

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -4,12 +4,19 @@ import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function Hero() {
   return (
-    <section id={"Hero"} className="select-none">
+    <section id={"Hero"} className="select-none relative">
       <img
-        className=" w-screen h-screen object-cover object-center"
+        className="w-screen h-screen object-cover object-center"
         src="/joss-portada.webp"
         alt="Joss"
       />
+      <div className="absolute bottom-20 left-[50%] transform -translate-x-2/4 -translate-y-2/4">
+        <a href="mailto:josselyncastroactriz@gmail.com">
+          <p className="text-2xl rounded-full backdrop-blur-sm px-6 py-2 cursor-pointer">
+            josselyncastroactriz@gmail.com
+          </p>
+        </a>
+      </div>
     </section>
   );
 }
