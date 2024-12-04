fe1daac133eda816c7692cc0a085ea783b3a7705
Fran <=>
2024-12-04 23:17:25 +0100

    Hover redes sociales

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import InstagramIcon from "../Icons/InstagramIcon";
-import YoutubeIcon from "../Icons/YoutubeIcon";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
-
+import { faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+/* import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+ */
 export default function Hero() {
   return (
     <section id={"Hero"} className="select-none relative">
@@ -12,25 +14,31 @@ export default function Hero() {
         src="/joss-portada.webp"
         alt="Joss"
       />
-      <div className="flex flex-col items-center gap-3 absolute bottom-16 left-[50%] transform -translate-x-2/4 -translate-y-2/4">
+      <div className="flex flex-col items-center gap-3 absolute bottom-2 left-[50%] transform -translate-x-2/4 -translate-y-2/4">
         <a href="mailto:josselyncastroactriz@gmail.com">
           <p className="text-white font-thin text-lg md:text-2xl rounded-full backdrop-blur-sm px-6 py-2 cursor-pointer">
             josselyncastroactriz@gmail.com
           </p>
         </a>
-        <div className="flex gap-6">
-          <a href="https://www.youtube.com/@josselyncastrogarces2094" target="_blank">
-            <YoutubeIcon color={"white"} size={"32px"} />
+        <div className="flex gap-4">
+          <a
+            href="https://www.youtube.com/@josselyncastrogarces2094"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              className="text-3xl md:text-4xl cursor-pointer hover:text-[#c4302b]"
+              icon={faYoutube}
+            />
           </a>
           <a href="https://www.instagram.com/josselyncg/" target="_blank">
-            <InstagramIcon
-              color={"white"}
-              size={"32px"}
+            <FontAwesomeIcon
+              className="text-3xl md:text-4xl cursor-pointer hover:bg-gradient-to-r from-[#833ab4] via-[#fd1d1d] to-[#fcb045] rounded-lg"
+              icon={faInstagram}
             />
           </a>
         </div>
       </div>
-{/*       <FontAwesomeIcon
+      {/*       <FontAwesomeIcon
         icon={faChevronDown}
         className="absolute text-[32px] bottom-1 left-[50%] opacity-30 transform -translate-x-2/4 -translate-y-2/4"
       /> */}
