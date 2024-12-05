dd77df7dddb95beefea2695ba295f093196e6d02
Fran <=>
2024-12-05 23:47:22 +0100

    Footer

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <footer id={"footer"}>Footer</footer>
-  )
+    <footer
+      id={"footer"}
+      className="bg-white text-black py-4 flex items-center justify-center"
+    >
+      <p className="text-lg">
+        designed by{" "}
+        <a href="https://webeo.es" target="_blank">
+          <span className="text-orange-900 font-semibold cursor-pointer hover:underline">
+            webeo
+          </span>
+        </a>
+      </p>
+    </footer>
+  );
 }
