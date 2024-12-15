c9f938284ae0dbf66b4267f5e6a717626be2d881
Fran <=>
2024-12-15 22:18:09 +0100

    Sombreado en portada

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faCalendarCheck, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 /* import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
  */
 export default function Hero() {
@@ -11,10 +12,11 @@ export default function Hero() {
         src="/joss-portada.webp"
         alt="Joss"
       />
-      <div className="flex flex-col items-center gap-3 absolute -bottom-2 md:bottom-2 left-[50%] transform -translate-x-2/4 -translate-y-2/4">
+      <div className="flex flex-col items-center gap-3 absolute -bottom-2 md:bottom-2 left-[50%] transform -translate-x-2/4 -translate-y-2/4 z-10">
         <a href="mailto:josselyncastroactriz@gmail.com">
-          <p className="text-white font-thin text-lg md:text-2xl rounded-full backdrop-blur-sm px-6 py-2 cursor-pointer">
-            josselyncastroactriz@gmail.com
+          <p className="group flex justify-center items-center gap-4 text-white font-thin text-lg md:text-2xl rounded-full backdrop-blur-sm px-6 py-2 cursor-pointer">
+            <FontAwesomeIcon className="text-[20px] group-hover:text-[#cf6e1fe1] transition-colors delay-100" icon={faEnvelope} />
+            <span>josselyncastroactriz@gmail.com</span>
           </p>
         </a>
         <div className="flex gap-4">
@@ -23,13 +25,13 @@ export default function Hero() {
             target="_blank"
           >
             <FontAwesomeIcon
-              className="text-white text-4xl cursor-pointer hover:text-[#c4302b]"
+              className="text-white text-4xl cursor-pointer hover:text-[#c4302b] transition-colors delay-100"
               icon={faYoutube}
             />
           </a>
           <a href="https://www.instagram.com/josselyncg/" target="_blank">
             <FontAwesomeIcon
-              className="text-white text-4xl cursor-pointer hover:bg-gradient-to-r from-[#833ab4] via-[#fd1d1d] to-[#fcb045] rounded-lg"
+              className="text-white text-4xl cursor-pointer hover:bg-gradient-to-r from-[#833ab4] via-[#fd1d1d] to-[#fcb045] rounded-lg transition-colors delay-100"
               icon={faInstagram}
             />
           </a>
@@ -39,6 +41,7 @@ export default function Hero() {
         icon={faChevronDown}
         className="absolute text-[32px] bottom-1 left-[50%] opacity-30 transform -translate-x-2/4 -translate-y-2/4"
       /> */}
+      <div className="absolute bottom-0 w-full h-[20%] bg-gradient-to-t from-[#000000] from-20% via-[#000000ce] via-30% to-[#00000000] to-100%" />
     </section>
   );
 }
