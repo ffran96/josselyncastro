66db08819dad07eee15186d4024f86a70cf9861d
Fran <=>
2024-12-25 01:26:43 +0100

    Volviendo al import relativo para las BigImages

--- src/app/data/images.jsx
+++ src/app/data/images.jsx
@@ -1,11 +1,11 @@
-import Image1 from "../../../public/josselyn-castro-actriz-gallerytb-1.webp";
-import Image2 from "../../../public/josselyn-castro-actriz-gallerytb-2.webp";
-import Image3 from "../../../public/josselyn-castro-actriz-gallerytb-3.webp";
-import Image4 from "../../../public/josselyn-castro-actriz-gallerytb-4.webp";
-import Image5 from "../../../public/josselyn-castro-actriz-gallerytb-5.webp";
-import Image6 from "../../../public/josselyn-castro-actriz-gallerytb-6.webp";
-import Image7 from "../../../public/josselyn-castro-actriz-gallerytb-7.webp";
-import Image8 from "../../../public/josselyn-castro-actriz-gallerytb-8.webp";
+import Image1 from "../../../public/josselyn-castro-actriz-gallerytb-1.webp"
+import Image2 from "../../../public/josselyn-castro-actriz-gallerytb-2.webp"
+import Image3 from "../../../public/josselyn-castro-actriz-gallerytb-3.webp"
+import Image4 from "../../../public/josselyn-castro-actriz-gallerytb-4.webp"
+import Image5 from "../../../public/josselyn-castro-actriz-gallerytb-5.webp"
+import Image6 from "../../../public/josselyn-castro-actriz-gallerytb-6.webp"
+import Image7 from "../../../public/josselyn-castro-actriz-gallerytb-7.webp"
+import Image8 from "../../../public/josselyn-castro-actriz-gallerytb-8.webp"
 
 import BigImage1 from "../../../public/josselyn-castro-actriz-gallery-1.jpg";
 import BigImage2 from "../../../public/josselyn-castro-actriz-gallery-2.jpg";
@@ -18,56 +18,56 @@ import BigImage8 from "../../../public/josselyn-castro-actriz-gallery-8.jpg";
 
 const GalleryImages = [
   {
-    largeURL: BigImage1,
-    altThumbnail: "Josselyn Castro - Actriz",
+    largeURL: "/josselyn-castro-actriz-gallery-1.jpg",
+    altThumbnail:"Josselyn Castro - Actriz",
     thumbnailURL: Image1,
     width: 2500,
     height: 3750,
   },
   {
-    largeURL: BigImage2,
+    largeURL: "/josselyn-castro-actriz-gallery-2.jpg",
     altThumbnail: "Josselyn Castro - Actriz",
     thumbnailURL: Image2,
     width: 2500,
     height: 3750,
   },
   {
-    largeURL: BigImage3,
+    largeURL: "/josselyn-castro-actriz-gallery-3.jpg",
     altThumbnail: "Josselyn Castro - Actriz",
     thumbnailURL: Image3,
     width: 2500,
     height: 3750,
   },
   {
-    largeURL: BigImage4,
+    largeURL: "/josselyn-castro-actriz-gallery-4.jpg",
     altThumbnail: "Josselyn Castro - Actriz",
     thumbnailURL: Image4,
     width: 2500,
     height: 3750,
   },
   {
-    largeURL: BigImage5,
+    largeURL: "/josselyn-castro-actriz-gallery-5.jpg",
     altThumbnail: "Josselyn Castro - Actriz",
     thumbnailURL: Image5,
     width: 2500,
     height: 3750,
   },
   {
-    largeURL: BigImage6,
+    largeURL: "/josselyn-castro-actriz-gallery-6.jpg",
     altThumbnail: "Josselyn Castro - Actriz",
     thumbnailURL: Image6,
     width: 2500,
     height: 3750,
   },
   {
-    largeURL: BigImage7,
+    largeURL: "/josselyn-castro-actriz-gallery-7.jpg",
     altThumbnail: "Josselyn Castro - Actriz",
     thumbnailURL: Image7,
     width: 2500,
     height: 3750,
   },
   {
-    largeURL: BigImage8,
+    largeURL: "/josselyn-castro-actriz-gallery-8.jpg",
     altThumbnail: "Josselyn Castro - Actriz",
     thumbnailURL: Image8,
     width: 2500,
@@ -98,3 +98,5 @@ Images.map(({ Image, Bigimage }) => {
 }); */
 
 export default GalleryImages;
+
+
