b87b49fec4e8eb90421bc82c03918e99321fd03b
ffran96 <75839722+ffran96@users.noreply.github.com>
2025-01-07 15:17:10 +0100

    Actualizar Conoceme.jsx
    Se actualiza la bio

--- src/app/Components/Conoceme.jsx
+++ src/app/Components/Conoceme.jsx
@@ -7,40 +7,15 @@ export default function Conoceme() {
       className="flex flex-col lg:flex-row items-center justify-between lg:max-w-7xl mx-auto"
     >
       <div className="px-3 lg:px-0 pt-32 lg:max-w-xl [&>p]:mb-4 [&>p]:leading-7	 [&>p]:text-lg [&>p]:text-pretty">
-        <h2 className="text-3xl font-semibold mb-3">Mi historia</h2>
+        <h2 className="text-3xl font-semibold mb-3">Conóceme</h2>
         <p>
-          En la adolescencia, mis aspiraciones evolucionaron. Al concluir la
-          educación secundaria, decidí optar por una carrera &#34;realista&#34;: la
-          abogacía. No obstante, a los 12 años, me recuerdo buscando escuelas de
-          interpretación por internet, mi pasión por el arte dramático seguía
-          estando ahí. A los 18 años, decidí independizarme y trabajar para
-          costearme la carrera de derecho y mis clases de teatro, las cuales
-          realizaba tres días a la semana y fueron la principal razón de que
-          saltara al mundo laboral.
+          Desde muy pequeña me he sentido atraída por el arte de la interpretación. Siguiendo mi vocación me formé en la Escuela Superior de Arte Dramático de Valencia, estudios que he enriquecidocon un entrenamiento actoral continuo en el centro Atelier del Drama y con talleres especializados en lectura y análisis de textos, creación de personajes e interpretación ante la cámara.
         </p>
         <p>
-          A pesar de mis esfuerzos por conciliar ambos estudios y el trabajo en
-          la hostelería, los textos de actuación siempre eclipsaban a las leyes
-          y apuntes de derecho, así que pausé mi formación como actriz, pero la
-          llamada de los focos y las tablas nunca cesó. En el tercer año de
-          derecho, la certeza de que mi lugar estaba frente a una cámara o sobre
-          un escenario se hizo innegable. Pero persistí y concluí mi primera
-          carrera como jurista antes de lanzarme de lleno a mi verdadera
-          vocación.
+         Estas experiencias me han proporcionado una sólida técnica actoral al haber abordado personajes y textos complejos de autores como Chéjov, Strindberg y Tennessee Williams. Mi trayectoria incluye producciones teatrales, spots publicitarios, videoclips y pequeños proyectos audiovisuales.
         </p>
         <p>
-          En los últimos ocho años, me he formado con rigor, cursando los
-          estudios en la Escuela Superior de Arte Dramático y entrenándome
-          simultáneamente en el Atelier del Drama dónde he descubierto a los
-          grandes autores como Shakespeare, Chéjov, Strindberg o T. Williams.
-          Además junto con mis compañeros de promoción he fundado la asociación
-          cultural Comboi estrenando nuestra primera obra teatral en 2023. Todo
-          ello no ha hecho sino que acrecentar mis ganas de dedicar mi vida a la
-          actuación
-        </p>
-        <p>
-          Ahora, tras años de dedicación y aprendizaje, me siento lista para
-          salir al mundo y ver crecer a mi actriz.
+          Soy una actriz disciplinada, siempre hambrienta de conocimiento y deseosa de encontrar proyectos en los que canalizar mi pasión por este oficio y seguir creciendo como artista.
         </p>
       </div>
       <div className="">
