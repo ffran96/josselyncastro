e40813c47ce21d19b6edbdfa276a02868c8fe855
Fran <=>
2025-01-12 00:43:14 +0100

    se reduce el ancho maximo para los dispositivos mas pequeños

--- src/app/Components/ContentSection.jsx
+++ src/app/Components/ContentSection.jsx
@@ -4,7 +4,7 @@ export default function ContentSection({ children, SectionId }) {
   return (
     <section
       id={SectionId}
-      className="max-w-[400px] sm:max-w-[550px] md:max-w-[600px] lg:max-w-5xl xl:max-w-7xl mx-auto"
+      className="max-w-[365px] sm:max-w-[550px] md:max-w-[600px] lg:max-w-5xl xl:max-w-7xl mx-auto"
     >
       {children}
     </section>
