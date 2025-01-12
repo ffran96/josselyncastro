e2388dad0a6d65fafa148982d4fb7ba27cb46bd7
Fran <=>
2025-01-12 14:43:18 +0100

    Se añaden keywords y og-image

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,24 @@ const inter = Lora({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Josselyn Castro - Actriz profesional en Valencia",
-  description: "Actriz profesional en Valencia. Formación y experiencia en teatro, audiovisuales y publicidad. ¡Descubre mi trabajo!😊",
+  description:
+    "Actriz profesional en Valencia. Formación y experiencia en teatro, audiovisuales y publicidad. ¡Descubre mi trabajo!😊",
+  keywords: [
+    "Actriz en Valencia",
+    "Actriz profesional",
+    "Actuación teatral",
+    "Casting para actriz",
+    "Interpretación ante la cámara",
+    "Creación de personajes",
+    "Publicidad y videoclips",
+    "Actriz para audiovisuales",
+    "Teatro y audiovisuales",
+    "Actriz española",
+  ],
+  openGraph: {
+    images:
+      "https://www.josselyncastro.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fjoss-portada.8a87a388.webp&w=3840&q=100",
+  },
 };
 
 export default function RootLayout({ children }) {
