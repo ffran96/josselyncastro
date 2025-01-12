a437dfcd5cdcd313c7daeb701a57872ec301e0f3
Fran <=>
2025-01-12 12:27:40 +0100

    se actualiza el mail

--- src/app/data/RRSS.jsx
+++ src/app/data/RRSS.jsx
@@ -1,7 +1,7 @@
 const RRSS = {
   instagram: "josselyncg",
   youtube: "@josselyncastrogarces2094",
-  email: "josselyncastroactriz@gmail.com",
+  email: "contacto@josselyncastro.com",
 };
 
 export default RRSS;
