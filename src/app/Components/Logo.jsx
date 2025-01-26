086dfbbc4c72cefa2bbc9dbb329fc1359c04d7d5
Fran <=>
2025-01-26 14:02:02 +0100

    Cambio color texto logo

--- src/app/Components/Logo.jsx
+++ src/app/Components/Logo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Logo() {
   return (
-    <div className="flex flex-row gap-1 text-slate-100">
+    <div className="flex flex-row gap-1 text-[#ffffff]">
       <span className="text-3xl font-extralight">Josselyn</span>
       <span className="text-3xl font-extralight">Castro</span>
     </div>
