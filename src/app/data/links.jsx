d04d061f67db4dfc5dc81c27e7cf38d6bf1734f9
Fran <=>
2025-01-26 14:02:51 +0100

    Data en formato oración

--- src/app/data/links.jsx
+++ src/app/data/links.jsx
@@ -1,9 +1,9 @@
 const Enlaces = [
-  { id: "0", name: "CONÓCEME", link: "#conoceme" },
-  { id: "1", name: "BOOK", link: "#book" },
-  { id: "2", name: "VIDEOBOOK", link: "#video-book" },
-  { id: "3", name: "CURRÍCULUM", link: "#cv" },
-  { id: "4", name: "CONTACTO", link: "#contacto" },
+  { id: "0", name: "Conóceme", link: "#conoceme" },
+  { id: "1", name: "Book", link: "#book" },
+  { id: "2", name: "Videobook", link: "#video-book" },
+  { id: "3", name: "Currículum", link: "#cv" },
+  { id: "4", name: "Contacto", link: "#contacto" },
 ];
 
 export default Enlaces;
