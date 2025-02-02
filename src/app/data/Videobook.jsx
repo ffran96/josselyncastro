77f52d404b5a72d5cbf97b0e99a8a8ed88f03be9
Fran <=>
2025-02-02 11:21:09 +0100

    Posters en webp.

--- src/app/data/Videobook.jsx
+++ src/app/data/Videobook.jsx
@@ -3,20 +3,20 @@ const Videos = [
     id: 1,
     title: "Spot para Mercadona",
     src: "spot-mercadona.mp4",
-    thumbnail: "poster-mercadona.jpg"
+    thumbnail: "poster-mercadona.webp"
 
   },
   {
     id: 2,
     title: "Escena de la película Historia de un matrimonio",
     src: "historia-de-un-matrimonio.mp4",
-    thumbnail: "poster-historia-de-un-matrimonio.jpg"
+    thumbnail: "poster-historia-de-un-matrimonio.webp"
   },
   {
     id: 3,
     title: "Escena de la obra de teatro La Gaviota",
     src: "la-gaviota.mp4",
-    thumbnail: "poster-la-gaviota.jpg"
+    thumbnail: "poster-la-gaviota.webp"
   },
 ];
 
