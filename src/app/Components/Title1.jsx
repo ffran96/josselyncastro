0bdb847f6839a72cd11f1e2e4f8be64f722c5ce8
Fran <=>
2025-02-02 15:26:03 +0100

    Cambio tamaño logo y cambio en navbar.

--- src/app/Components/Title1.jsx
+++ src/app/Components/Title1.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Title1({ children, Class }) {
   return (
-    <h2
+    <h1
       className={`${Class} text-3xl 2xl:text-5xl font-extralight tracking-tighter uppercase`}
     >
       {children}
-    </h2>
+    </h1>
   );
 }
