855c1d166f6d584086b3c3b1b90c1e4578072544
Fran <=>
2025-02-06 23:20:07 +0100

    Quitar Next Config para Cloudflare

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [{ key: "CF-Connecting-IP", value: "IP del usuario" }],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
